8de831deff59920b75f76ccc50360b374229ddba
GitsOfVivek <singhvivek1999@yahoo.com>
2023-01-28 08:22:24 +0530

    extract time & earning as static variable

--- MaxProfitProblem.java
+++ MaxProfitProblem.java
@@ -9,6 +9,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MaxProfitProblem {
+
+    static int cParkTime = 10, cParkEarning = 3000;
+    static int theaterTime = 5, theaterEarning = 1500;
+    static int pubTime = 4, pubEarning = 1000;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int time = sc.nextInt();
@@ -18,20 +23,21 @@ public class MaxProfitProblem {
             System.out.println("Earnings : $0");
             return;
         }
+
         ArrayList<int[]> listOfResult = new ArrayList<>();
         int totalEarning = 0;
         int maxEarning = Integer.MIN_VALUE;
 
-        int cPark = time / 10;
+        int cPark = time / cParkTime;
 
         while (cPark >= 0) {
-            int timeLeft1 = time - (cPark * 10);
-            int theater = timeLeft1 / 5;
+            int timeLeft1 = time - (cPark * cParkTime);
+            int theater = timeLeft1 / theaterTime;
             while (theater >= 0) {
-                int timeLeft2 = timeLeft1 - (theater * 5);
+                int timeLeft2 = timeLeft1 - (theater * theaterTime);
                 int pub = 0;
-                if (timeLeft2 > 4) {
-                    pub = timeLeft2 / 4;
+                if (timeLeft2 > pubTime) {
+                    pub = timeLeft2 / pubTime;
                 }
                 totalEarning = findEarning(time, cPark, theater, pub);
                 int[] result = new int[4];
@@ -60,15 +66,15 @@ public class MaxProfitProblem {
     public static int findEarning(int time, int cPark, int theater, int pub) {
         int earning = 0;
         for (int i = 1; i <= cPark; i++) {
-            earning += (time - (i * 10)) * 6000;
+            earning += (time - (i * cParkTime)) * cParkEarning;
         }
-        time -= cPark * 10;
+        time -= cPark * cParkTime;
         for (int i = 1; i <= theater; i++) {
-            earning += (time - (i * 5)) * 1500;
+            earning += (time - (i * theaterTime)) * theaterEarning;
         }
-        time -= theater * 5;
+        time -= theater * theaterTime;
         for (int i = 1; i <= pub; i++) {
-            earning += (time - (i * 4)) * 1000;
+            earning += (time - (i * pubTime)) * pubEarning;
         }
         return earning;
     }
